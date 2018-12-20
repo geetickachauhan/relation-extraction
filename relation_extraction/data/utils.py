9425953eb211d64aa7b51ca7f3d9d5f82f70c631
Geeticka Chauhan <gchau005@fiu.edu>
2018-12-20 11:07:33 -0500

    pooling idea 2 - e1 goes with second piece, e2 with third piece and first piece by itself

--- relation_extraction/data/utils.py
+++ relation_extraction/data/utils.py
@@ -564,8 +564,8 @@ def vectorize(config, data, word_dict):
 
     dist1, dist2, num_pos = relative_distance(num_data, max_sen_len, e1_pos, e2_pos)
     
-    position1 = list(map(lambda x: x[1], e1_pos))
-    position2 = list(map(lambda x: x[1], e2_pos))
+    position1 = list(map(lambda x: x[0], e1_pos))
+    position2 = list(map(lambda x: x[0], e2_pos))
     if config.use_elmo is True: 
         return sents_vec, np.array(relations).astype(np.int64), e1_vec, e2_vec, dist1, dist2, \
     padded_elmo_embeddings, position1, position2
--- relation_extraction/models/model.py
+++ relation_extraction/models/model.py
@@ -71,9 +71,9 @@ class Model(object):
             print("elmo weighted sum", elmo_weighted_sum.shape)
             tf.summary.histogram("elmo_weighted_sum", elmo_weighted_sum)
 
-        tf.summary.histogram("word_embedding_matrix", embed)
-        tf.summary.histogram("position1_embedding_matrix", pos1_embed)
-        tf.summary.histogram("position2_embedding_matrix", pos2_embed)
+        #tf.summary.histogram("word_embedding_matrix", embed)
+        #tf.summary.histogram("position1_embedding_matrix", pos1_embed)
+        #tf.summary.histogram("position2_embedding_matrix", pos2_embed)
 
 
         # embdding lookup
@@ -241,11 +241,11 @@ class Model(object):
         width = tf.Variable(bwc_conv.shape[1], dtype=tf.int32) # total width (i.e. max sentence length)
 
         b_slice_from = [i, 0, 0]
-        b_slice_size = tf.concat([[1], l_ind + 1, [channels_count]], 0)
-        m_slice_from = tf.concat([[i], l_ind + 1, [0]], 0)
+        b_slice_size = tf.concat([[1], l_ind, [channels_count]], 0)
+        m_slice_from = tf.concat([[i], l_ind, [0]], 0)
         m_slice_size = tf.concat([[1], r_ind - l_ind, [channels_count]], 0)
-        a_slice_from = tf.concat([[i], r_ind + 1, [0]], 0)
-        a_slice_size = tf.concat([[1], width - r_ind - 1, [channels_count]], 0)
+        a_slice_from = tf.concat([[i], r_ind, [0]], 0)
+        a_slice_size = tf.concat([[1], width - r_ind, [channels_count]], 0)
 
         bwc_split_b = tf.slice(bwc_conv, b_slice_from, b_slice_size)
         bwc_split_m = tf.slice(bwc_conv, m_slice_from, m_slice_size)
@@ -253,7 +253,7 @@ class Model(object):
        
 
         pad_b = tf.concat([[[0,0]],
-                            tf.reshape(tf.concat([width - l_ind - 1, [0]], 0), shape=[1,2]),
+                            tf.reshape(tf.concat([width - l_ind, [0]], 0), shape=[1,2]),
                             [[0,0]]],
                             axis=0)
         
@@ -263,7 +263,7 @@ class Model(object):
                             axis=0)
 
         pad_a = tf.concat([[[0,0]],
-                            tf.reshape(tf.concat([r_ind + 1, [0]], 0), shape=[1,2]),
+                            tf.reshape(tf.concat([r_ind, [0]], 0), shape=[1,2]),
                             [[0,0]]],
                             axis=0)
 
